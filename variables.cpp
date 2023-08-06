76cd5a8f7d66551e9d0c4b7e495367eaa68eedba
Aman Lath <85346421+Amanlath1@users.noreply.github.com>
2023-08-06 21:06:40 +0000

    variable.cpp updated

--- variables.cpp
+++ variables.cpp
@@ -3,14 +3,18 @@ using namespace std;
 
 int main(){
     int a =1;
-    char name = 'A';
+    char Char = 'A';
     bool answer = true;
     float decimal = 10.1;
+    double deeci = 1.1;
+    string name = "Github";
 
     cout<< a << endl;
-    cout<< name << endl;
+    cout<< Char << endl;
     cout<< answer << endl;
-    cout<< decimal <<endl;
+    cout<< decimal << endl;
+    cout<< deeci << endl;
+    cout<< name << endl;
 
     return 0;
 }
