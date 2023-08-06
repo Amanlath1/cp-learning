3934ec552eb9d193c50a1f5eb07cf99aa3d59634
Aman Lath <85346421+Amanlath1@users.noreply.github.com>
2023-08-06 20:54:12 +0000

    variables.cpp added

---
+++ variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a =1;
+    char name = 'A';
+    bool answer = true;
+    float decimal = 10.1;
+
+    cout<< a << endl;
+    cout<< name << endl;
+    cout<< answer << endl;
+    cout<< decimal <<endl;
+
+    return 0;
+}
